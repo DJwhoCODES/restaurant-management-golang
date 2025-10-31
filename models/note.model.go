698668d73e74caf4149f02535ca1a35458d135ae
DJwhoCODES <jaindevanshu824@gmail.com>
2025-11-01 01:43:08 +0530

    designed schemas

--- models/note.model.go
+++ models/note.model.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Note struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Title      *string            `json:"title" validate:"required,min=2,max=100"`
+	Text       *string            `json:"text" validate:"required,min=2,max=1000"`
+	Created_At time.Time          `json:"created_at"`
+	Updated_At time.Time          `json:"updated_at"`
+	Note_Id    string             `json:"note_id"`
+}
