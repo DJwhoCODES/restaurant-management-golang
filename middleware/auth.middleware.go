2a2a91ad22c318389db9a72dde36dfde5da37cb5
DJwhoCODES <jaindevanshu824@gmail.com>
2025-11-03 00:08:08 +0530

    completed

--- middleware/auth.middleware.go
+++ middleware/auth.middleware.go
@@ -1,1 +1,34 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/djwhocodes/restaurant_management/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+
+		if clientToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No authorization token provided"})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.FirstName)
+		c.Set("last_name", claims.LastName)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
+	}
+}
